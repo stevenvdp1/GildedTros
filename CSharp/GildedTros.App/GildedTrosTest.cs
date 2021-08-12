28fb372b0f621cb7018ae44b36512d1921d27155
Steven Van de Poel <stevenvandepoel@gmail.com>
2021-08-12 21:57:24 +0200

    First tests

--- CSharp/GildedTros.App/GildedTrosTest.cs
+++ CSharp/GildedTros.App/GildedTrosTest.cs
@@ -6,12 +6,25 @@ namespace GildedTros.App
     public class GildedTrosTest
     {
         [Fact]
-        public void foo()
+        public void QualityAndSellInnDecrease()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedTros app = new GildedTros(Items);
+            app.UpdateQuality();
+            Assert.Equal("foo", Items[0].Name);
+            Assert.Equal(9, Items[0].Quality);
+            Assert.Equal(9, Items[0].SellIn);
+        }
+
+        [Fact]
+        public void SellInnDecreasesQualityCannotBeNegative()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedTros app = new GildedTros(Items);
             app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
+            Assert.Equal("foo", Items[0].Name);
+            Assert.Equal(-1, Items[0].SellIn);
+            Assert.Equal(0, Items[0].Quality);
         }
     }
 }
--- TypeScript/src/item.ts
+++
@@ -1,8 +0,0 @@
-export class Item {
-  constructor(public name: string, public sellIn: number, public quality: number) {
-}
-
-public toString(): string {
-    return `${this.name}, ${this.sellIn}, ${this.quality}`;
-}
-}
